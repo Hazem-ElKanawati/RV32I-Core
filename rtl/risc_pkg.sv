57d036774b119a21e218199636b627b76f0d8046
Hazem Mohamed ElKanawati <hazemelkanawati@gmail.com>
2026-07-08 21:31:02 +0300

    R,I,S type and writeback source enums

--- rtl/risc_pkg.sv
+++ rtl/risc_pkg.sv
@@ -54,26 +54,50 @@ package risc_pkg;
   // B-Type Instructions (Funct3)
   // --------------------------------------------------------
   typedef enum logic [2:0] {
-    BEQ = 3'h0,
-    BNE = 3'h1,
-    BLT = 3'h4,
-    BGE = 3'h5,
-    BLTU = 3'h6,
-    BGEU = 3'h7
+    B_BEQ = 3'h0,
+    B_BNE = 3'h1,
+    B_BLT = 3'h4,
+    B_BGE = 3'h5,
+    B_BLTU = 3'h6,
+    B_BGEU = 3'h7
   } b_type_instr_t;
 
   // --------------------------------------------------------
   // R-Type Instructions (Funct7[5], Funct3)
   // --------------------------------------------------------
- 
- 
- 
- 
+  typedef enum logic  [3:0]
+  {
+    R_ADD = 4'h0,
+    R_SUB = 4'h8,
+    R_SLL = 4'h1,
+    R_SLT = 4'h2,
+    R_SLTU = 4'h3,
+    R_XOR = 4'h4,
+    R_SRL = 4'h5,
+    R_SRA = 4'hD,
+    R_OR = 4'h6,
+    R_AND = 4'h7
+  } r_type_instr_t;
 
   // --------------------------------------------------------
   // I-Type Instructions (Opcode[4], Funct3)
   // --------------------------------------------------------
-  
+    typedef enum logic [3:0]
+    {
+      I_LB = 4'h0,
+      I_LH = 4'h1,
+      I_LW = 4'h2,
+      I_LBU = 4'h4,
+      I_LHU = 4'h5,
+      I_ADDI = 4'h8,
+      I_SLTI = 4'hA,
+      I_SLTIU = 4'hB,
+      I_XORI = 4'hC,
+      I_ORI = 4'hE,
+      I_ANDI = 4'hF,
+      I_SLLI = 4'h9,
+      I_SRLI_SRAI = 4'hD
+    } i_type_instr_t;
   
   
   
@@ -81,15 +105,24 @@ package risc_pkg;
   // --------------------------------------------------------
   // S-Type Instructions (Funct3)
   // --------------------------------------------------------
+  typedef enum logic [2:0] {
+    S_SB = 3'h0,
+    S_SH = 3'h1,
+    S_SW = 3'h2
+  } s_type_instr_t;
   
-  
- 
   
   
   // --------------------------------------------------------
   // Register File Writeback Sources
   // --------------------------------------------------------
- 
+  typedef enum logic [1:0]
+  {
+    WB_SRC_ALU = 2'b00,
+    WB_SRC_MEM = 2'b01,
+    WB_SRC_IMM = 2'b10,
+    WB_SRC_PC = 2'b11
+  } wb_src_t;
  
  
  
